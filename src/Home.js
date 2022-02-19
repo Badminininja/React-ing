8026e6f84c0516e66c7e723310e368a1f95b8eed
Joseph M <josephnm911@gmail.com>
2022-02-18 17:09:36 -0800

    Created buttons with click events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,20 @@
 const Home = () => {
+    //The e is there to show properties of the output from the buttons
+    //e.target is to just show the exact code where the output is coming from
+    const handleClickAgain = (name, e) => {
+
+        console.log('You have Clicked a successful button ' + name, e.target);
+    }
+    const handleClick = (e) => {
+
+        console.log('oh, Hello', e);
+    }
+
     return ( 
         <div className = "home">
             <h1>HomePage</h1>
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick = {(e) => handleClickAgain('Joseph', e)}> The Second Button</button>
         </div>
 
      );
